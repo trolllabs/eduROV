ed9f7e759355450c95937bc2eb5257fe6116f25a
Martin Loeland <martinloland@gmail.com>
2018-01-31 21:57:05 +0100

    arg error

--- start.py
+++ start.py
@@ -9,11 +9,11 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Stream video from picamera to machine on same network')
     parser.add_argument('role', choices=choices, help='which role to play')
     parser.add_argument('ip', help='IP address the server listens at and '
-                                   'client sends to')
+                                   'client sends to', type=str)
     parser.add_argument('-p', metavar='PORT', type=int, default=1060,
                         help='IP port (default 1060)')
     parser.add_argument('-r', metavar='RESOLUTION', type=str, default='VGA',
                         help='resolution (default VGA)')
     args = parser.parse_args()
     function = choices[args.role]
-    function(args.host, args.p, args.r)
+    function(args.ip, args.p, args.r)
