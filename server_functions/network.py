db85e1d548b317f2f920a63a924cfebe6ef42147
Martin Loeland <martinloland@gmail.com>
2018-02-12 00:52:29 +0100

    removed shit

--- server_functions/network.py
+++ server_functions/network.py
@@ -2,35 +2,8 @@ import struct
 import io
 import sys
 import socket
-import multiprocessing
 import pygame
 from PIL import Image
-
-def get_frame(connection, download_q):
-    image_stream = io.BytesIO()
-    # Read the length of the image as a 32-bit unsigned int. If the
-    # length is zero, quit the loop
-    while True:
-        image_len = \
-            struct.unpack('<L', connection.read(struct.calcsize('<L')))[0]
-        if not image_len:
-            break
-        # Read image data and convert: BytesIO > PIL.Image > pygame.image
-        image_stream.write(connection.read(image_len))
-        download_q.put(image_stream)
-        # Rewind byte stream
-        image_stream.seek(0)
-
-def jpg_to_rgb(screen_size, download_q, rgb_q):
-    while True:
-        PILframe = Image.open(download_q.get()).tobytes()
-        rgb_q.put(pygame.image.frombuffer(PILframe, screen_size, 'RGB'))
-
-def blit_bytes(screen, rgb_q):
-    while True:
-        # Update screen
-        screen.blit(rgb_q.get(), (0,0))
-        pygame.display.flip()
 
 
 def server(interface, port, resolution, fullscreen):
@@ -52,21 +25,25 @@ def server(interface, port, resolution, fullscreen):
     # Accept a single connection and make a file-like object out of it
     connection = server_socket.accept()[0].makefile('rb')
     try:
-        download_q = multiprocessing.Queue()
-        rgb_q = multiprocessing.Queue()
-        p1 = multiprocessing.Process(target=get_frame,
-                                     args=(connection, download_q))
-        p2 = multiprocessing.Process(target=jpg_to_rgb,
-                                     args=(screen_size, download_q, rgb_q))
-        p3 = multiprocessing.Process(target=blit_bytes,
-                                     args=(screen, rgb_q))
-        p1.start()
-        p2.start()
-        p3.start()
-
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT: sys.exit()
+            # Read the length of the image as a 32-bit unsigned int. If the
+            # length is zero, quit the loop
+            image_len = \
+                struct.unpack('<L', connection.read(struct.calcsize('<L')))[0]
+            if not image_len:
+                break
+            # Read image data and convert: BytesIO > PIL.Image > pygame.image
+            image_stream = io.BytesIO()
+            image_stream.write(connection.read(image_len))
+            PILframe = Image.open(image_stream).tobytes()
+            frame = pygame.image.fromstring(PILframe, screen_size, 'RGB')
+            # Update screen
+            screen.blit(frame, (0,0))
+            pygame.display.flip()
+            # Rewind byte stream
+            image_stream.seek(0)
     finally:
         connection.close()
         server_socket.close()
