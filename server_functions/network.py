78ea69c532affc87b1c6dc22a82d957a29f154d4
Martin Loeland <martinloland@gmail.com>
2018-02-12 23:45:20 +0100

    working good with classes

--- server_functions/network.py
+++ server_functions/network.py
@@ -4,35 +4,14 @@ import sys
 import socket
 import pygame
 from PIL import Image
-
-
-def check_keys(screen_size, fullscreen):
-    quit = False
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            quit = True
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:
-                quit = True
-            if event.key == pygame.K_RETURN:
-                if not fullscreen:
-                    pygame.display.set_mode(screen_size, pygame.FULLSCREEN)
-                    fullscreen = True
-                else:
-                    pygame.display.set_mode(screen_size)
-                    fullscreen = False
-    return quit, fullscreen
-
-
-def get_screen(screen_size, fullscreen):
-    if fullscreen:
-        return pygame.display.set_mode(screen_size, pygame.FULLSCREEN)
-    else:
-        return pygame.display.set_mode(screen_size)
+BLACK = 0, 0, 0
+from multiprocessing.pool import ThreadPool
 
 
 class Server(object):
     def __init__(self, ip, port):
+        self.client_connected = False
+        self.pool = ThreadPool(processes=1)
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.sock.bind((ip, port))
@@ -40,15 +19,20 @@ class Server(object):
         print('Listening at', self.sock.getsockname())
         print('Client should connect to {}'
               .format(socket.gethostbyname(socket.gethostname())))
-        # Accept a single connection and make a file-like object out of it
-        self.conn = self.sock.accept()[0].makefile('rb')
         self.image_stream = io.BytesIO()
+
+        self.async_connection = self.pool.apply_async(self.client_connection)
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         print('Shutting down server')
-        self.conn.close()
+        if self.client_connected:
+            self.conn.close()
         self.sock.close()
         sys.exit()
+
+    def client_connection(self):
+        self.conn = self.sock.accept()[0].makefile('rb')
+        self.client_connected = True
 
     def __enter__(self):
         return self
@@ -59,22 +43,64 @@ class Server(object):
             struct.unpack('<L', self.conn.read(struct.calcsize('<L')))[0]
         if not image_len:
             raise ConnectionError('Image length is None')
-        # Read image data and convert: BytesIO > PIL.Image > pygame.image
-        # image_stream = io.BytesIO()
         self.image_stream.write(self.conn.read(image_len))
         return self.image_stream
 
 
-def server(ip, port, resolution, fullscreen):
-    pygame.init()
-    screen_size = tuple([int(val) for val in resolution.split('x')])
-    screen = get_screen(screen_size, fullscreen)
+class Screen(object):
+    def __init__(self, screen_size, fullscreen=False):
+        self.screen_size = screen_size
+        pygame.init()
+        pygame.display.set_caption('eduROV')
+        if fullscreen:
+            fullscreen = pygame.FULLSCREEN
+        self.screen = pygame.display.set_mode(screen_size, fullscreen)
+        self.fullscreen = fullscreen
 
-    with Server(ip, port) as serve:
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        pass
+
+    def update(self, frame=None):
+        self.read_keys()
+        if frame:
+            self.screen.blit(frame, (0,0))
+        else:
+            self.screen.fill(BLACK)
+        pygame.display.flip()
+
+    def read_keys(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.quit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    self.quit()
+                if event.key == pygame.K_RETURN:
+                    self.toggle_fullscreen()
+
+    def quit(self):
+        sys.exit()
+
+    def toggle_fullscreen(self):
+        if self.fullscreen:
+            self.fullscreen = False
+        else:
+            self.fullscreen = pygame.FULLSCREEN
+        pygame.display.set_mode(self.screen_size, self.fullscreen)
+
+
+def server(ip, port, resolution, fullscreen):
+    screen_size = tuple([int(val) for val in resolution.split('x')])
+    screen = Screen(screen_size=screen_size, fullscreen=fullscreen)
+
+    with Server(ip, port) as server:
         while True:
-            exit, fullscreen = check_keys(screen_size, fullscreen)
-            if exit: break
-            pil_frame = Image.open(serve.img_stream()).tobytes()
-            frame = pygame.image.fromstring(pil_frame, screen_size, 'RGB')
-            screen.blit(frame, (0,0))
-            pygame.display.flip()
+            if server.client_connected:
+                pil_frame = Image.open(server.img_stream()).tobytes()
+                frame = pygame.image.fromstring(pil_frame, screen_size, 'RGB')
+                screen.update(frame)
+            else:
+                screen.update()
