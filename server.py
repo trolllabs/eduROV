e9758164398c6ac5e7a922a37f0a3bdbf7088709
Martin Loeland <martinloland@gmail.com>
2018-02-13 23:39:30 +0100

    large image test

--- server.py
+++ server.py
@@ -142,7 +142,7 @@ def print_server_ip():
 if __name__ == '__main__':
     print_server_ip()
 
-    with picamera.PiCamera(resolution='640x480', framerate=24) as camera:
+    with picamera.PiCamera(resolution='1296x972', framerate=24) as camera:
         output = StreamingOutput()
         camera.start_recording(output, format='mjpeg')
         try:
