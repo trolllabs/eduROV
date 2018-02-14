db34f0e34579a53fd3b815ab245b87122e7265ed
Martin Loeland <martinloland@gmail.com>
2018-02-14 13:01:05 +0100

    abs path 3

--- server.py
+++ server.py
@@ -10,9 +10,9 @@ import struct
 import os
 
 cwd = os.path.dirname(os.path.abspath(__file__))
-index_file = os.path.join(cwd,'index.html')
-css_file = os.path.join(cwd, '\web_content\style.css')
-script_file = os.path.join(cwd, '\web_content\script.js')
+index_file = os.path.join(cwd, 'index.html')
+css_file = os.path.join(cwd, './web_content/style.css')
+script_file = os.path.join(cwd, './web_content/script.js')
 
 with open(index_file, 'r') as f:
     html = f.read()
