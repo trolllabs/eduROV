4958798786a329d61d5c03631ee28fa1664f30ff
Martin Loeland <martinloland@gmail.com>
2018-02-20 20:41:30 +0100

    json loop concat 2

--- static/script.js
+++ static/script.js
@@ -34,8 +34,7 @@ function get_sensor(){
                 var left_text = "";
                 var sensor = JSON.parse(this.responseText);
                 for (var key in sensor) {
-                    left_text = left_text.concat(key + ": " sensor[key] +
-                    "<br />");
+                    left_text = left_text.concat(key + ": " sensor[key] + "<br />");
                 }
                 document.getElementById("sensordata").innerHTML = left_text;
             }
