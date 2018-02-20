048b123669b0c180161bbcb4a467335248958b36
Martin Loeland <martinloland@gmail.com>
2018-02-20 14:43:05 +0100

    pressed keys now in dict

--- static/script.js
+++ static/script.js
@@ -1,3 +1,21 @@
+var last_key;
+var pressed_keys = {};
+
+document.onkeydown = function(evt) {
+    evt = evt || window.event;
+    if (evt.keyCode != last_key){
+        pressed_keys[evt.keyCode] = 1;
+        console.log(pressed_keys)
+        lastkey = evt.keyCode;
+    }
+};
+
+document.onkeyup = function(evt) {
+    delete pressed_keys[evt.keyCode];
+    console.log(pressed_keys)
+    last_key = 0;
+};
+
 function resizeToMax(id){
     myImage = new Image()
     var img = document.getElementById(id);
