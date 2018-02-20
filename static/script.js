94b30c3900dfc5b5dc947cb14746085be6e402d9
Martin Loeland <martinloland@gmail.com>
2018-02-20 17:18:55 +0100

    interval sensordata 11

--- static/script.js
+++ static/script.js
@@ -34,7 +34,7 @@ function get_sensor(){
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
             if (this.readyState == 4 && this.status == 200) {
-                console.log(this.responseText);
+                document.getElementById("sensordata").innerHTML = this.responseText;
             }
         };
     xhttp.open("GET", "sensordata.json", true);
