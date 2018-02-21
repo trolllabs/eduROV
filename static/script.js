e0b4d34d92ecdd701c27ec0ab1e582b246bb4fc5
Martin Loeland <martinloland@gmail.com>
2018-02-21 11:30:53 +0100

    resize test 3

--- static/script.js
+++ static/script.js
@@ -75,6 +75,17 @@ function resizeToMax(id){
     }
 }
 
-window.onresize = function(event) {
-    console.log("I am resising");
-}
+var addEvent = function(object, type, callback) {
+    if (object == null || typeof(object) == 'undefined') return;
+    if (object.addEventListener) {
+        object.addEventListener(type, callback, false);
+    } else if (object.attachEvent) {
+        object.attachEvent("on" + type, callback);
+    } else {
+        object["on"+type] = callback;
+    }
+};
+
+addEvent(window, "resize", function(event) {
+  console.log('resized');
+});
