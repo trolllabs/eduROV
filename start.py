1426d6f2da1d3d08e0076a8e289bb3cce370feae
Martin Loeland <martinloland@gmail.com>
2018-02-28 16:27:26 +0100

    pyro simple test 5

--- start.py
+++ start.py
@@ -1,12 +1,13 @@
 import subprocess
 import multiprocessing
+import time
 from simple_start import start
 from client_one import client_one
 
 if __name__ == '__main__':
     # subprocess.Popen(['python', 'simple_start.py'], shell=False)
     server = multiprocessing.Process(target=start)
+    server.start()
+    time.sleep(5)
     client1 = multiprocessing.Process(target=client_one)
-    processes = [server, client1]
-    for p in processes:
-        p.start()
+    client1.start()
