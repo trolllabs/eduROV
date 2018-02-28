d2cdaaeecd1e9f7b26cb598ee3d1f704d45a4171
Martin Loeland <martinloland@gmail.com>
2018-02-28 15:05:49 +0100

    arrow test

--- manage_sense_hat.py
+++ manage_sense_hat.py
@@ -3,6 +3,19 @@ from support import detect_pi
 if detect_pi():
     from sense_hat import SenseHat
 
-sense = SenseHat()
+X = [255, 0, 0]  # Red
+O = [255, 255, 255]  # White
 
-sense.show_message("Hello world!")
+up = [
+    O, O, O, X, X, O, O, O,
+    O, O, X, X, X, X, O, O,
+    O, X, X, X, X, X, X, O,
+    X, X, O, X, X, O, X, X,
+    O, O, O, X, X, O, O, O,
+    O, O, O, X, X, O, O, O,
+    O, O, O, X, X, O, O, O,
+    O, O, O, X, X, O, O, O
+]
+
+sense = SenseHat()
+sense.set_pixels(up)
