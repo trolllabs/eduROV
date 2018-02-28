1c547363ee76c2b3630341f494524d802d381150
Martin Loeland <martinloland@gmail.com>
2018-02-28 16:21:15 +0100

    pyro simple test 3

--- start.py
+++ start.py
@@ -1,9 +1,4 @@
 import subprocess
-import time
 
 if __name__ == '__main__':
-    subprocess.Popen(['python', 'rov_server.py'], shell=False)
-    time.sleep(5)
-    subprocess.Popen(['python', 'client_one.py'], shell=False)
-    subprocess.Popen(['python', 'client_two.py'], shell=False)
-    time.sleep(10)
+    subprocess.Popen(['python', 'simple_start.py'], shell=False)
