4494056306c541756b42d978388f9a047192a312
Martin Loeland <martinloland@gmail.com>
2018-02-28 13:38:26 +0100

    client test file 2

--- pyro_client.py
+++ pyro_client.py
@@ -3,9 +3,10 @@ import Pyro4
 
 if __name__ == '__main__':
     with Pyro4.Proxy("PYRONAME:ROVServer") as rov:
-        with Pyro4.Proxy("PYRONAME:KeyManager") as keys:
-            print(keys.state('r'))
-            keys.keydown('r')
-            print(keys.state('r'))
-            keys.keyup('r')
-            print(keys.state('r'))
+        rov.shutdown()
+        # with Pyro4.Proxy("PYRONAME:KeyManager") as keys:
+        #     print(keys.state('r'))
+        #     keys.keydown('r')
+        #     print(keys.state('r'))
+        #     keys.keyup('r')
+        #     print(keys.state('r'))
