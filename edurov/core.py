e09b410b3759f879066f5b8daabfb59b8cffffd8
Martin Loeland <martinloland@gmail.com>
2018-03-19 22:51:40 +0100

    added core classes

---
+++ edurov/core.py
@@ -0,0 +1,13 @@
+class WebMethod(object):
+    def __init__(self):
+        pass
+
+
+class KeyConnection(object):
+    def __init__(self):
+        pass
+
+
+class RovConnection(object):
+    def __init__(self):
+        pass
