165542b62c2c2536180e73f501dbf835f79a2619
Martin Loeland <martinloland@gmail.com>
2018-03-20 17:06:27 +0100

    index fix 6

--- examples_edurov/edurov_web/start.py
+++ examples_edurov/edurov_web/start.py
@@ -85,14 +85,13 @@ def senser(debug=False):
 
 
 def main(video_resolution='1024x768', fps=30, server_port=8000, debug=False):
-    print(os.path.abspath(__file__))
     web_method = WebMethod(
         video_resolution=video_resolution,
         fps=fps,
         server_port=server_port,
         debug=debug,
         runtime_functions=[arduino, senser],
-        index_file = 'index.html'
+        index_file = os.path.join(os.path.dirname(__file__),'index.html')
     )
     web_method.serve()
 
