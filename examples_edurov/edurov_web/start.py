b3a9ad93bff95b01c02d78603cb05657a7ad0ce9
Martin Loeland <martinloland@gmail.com>
2018-03-20 17:01:01 +0100

    index fix 5

--- examples_edurov/edurov_web/start.py
+++ examples_edurov/edurov_web/start.py
@@ -10,6 +10,7 @@ from edurov import WebMethod
 from edurov.arduino import get_serial_connection, send_arduino, \
     receive_arduino, valid_arduino_string
 from edurov.utils import detect_pi
+import os
 
 if detect_pi():
     from sense_hat import SenseHat
@@ -84,6 +85,7 @@ def senser(debug=False):
 
 
 def main(video_resolution='1024x768', fps=30, server_port=8000, debug=False):
+    print(os.path.abspath(__file__))
     web_method = WebMethod(
         video_resolution=video_resolution,
         fps=fps,
