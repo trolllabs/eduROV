bfecda5b3fc35f328a2b7f74673e8f98edb85427
Martin Loeland <martinloland@gmail.com>
2018-03-20 22:43:42 +0100

    versjon 0.0.3

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 
 setup(
     name='edurov',
-    version='0.0.2',
+    version='0.0.3',
     description='A educational project for remotely operated vehicles',
     long_description=read('README.rst'),
     license='GPLv3',
