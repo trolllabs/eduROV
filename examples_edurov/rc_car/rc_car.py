992900a0264c49464f9339faffd65f3f8e922b0f
Martin Loeland <martinloland@gmail.com>
2018-04-19 12:35:04 +0200

    joystick behavior 4

--- examples_edurov/rc_car/rc_car.py
+++ examples_edurov/rc_car/rc_car.py
@@ -105,8 +105,8 @@ def control_motors():
     GPIO.setmode(GPIO.BCM)
     m1 = Motor(4, 18, pwm=True)
     m2 = Motor(12, 19, pwm=True)
-    normal = 1.0
-    turn = 0.7
+    normal = 0.8
+    turn = 0.8
     u = Button()
     d = Button()
     l = Button()
