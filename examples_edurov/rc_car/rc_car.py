031a17c41f46b38dab618b3379223f1d4c73175c
Martin Loeland <martinloland@gmail.com>
2018-04-22 15:11:28 +0200

    moved classes to own file

--- examples_edurov/rc_car/rc_car.py
+++ examples_edurov/rc_car/rc_car.py
@@ -1,112 +1,20 @@
 import argparse
 import os
-import time
 
 import Pyro4
 import RPi.GPIO as GPIO
+from .classes import Motor, Button
 
 from edurov import WebMethod
 
 
-class Motor(object):
-    pwm_frequency = 490
-
-    def __init__(self, a_pin, b_pin, reverse=False, pwm=False):
-        self.reverse = reverse
-        self.pwm = pwm
-        if not reverse:
-            self.a_pin = a_pin
-            self.b_pin = b_pin
-        else:
-            self.a_pin = b_pin
-            self.b_pin = a_pin
-        for pin in [a_pin, b_pin]:
-            GPIO.setup(pin, GPIO.OUT)
-
-        if self.pwm:
-            self.a_pwm = GPIO.PWM(self.a_pin, self.pwm_frequency)
-            self.b_pwm = GPIO.PWM(self.b_pin, self.pwm_frequency)
-            self.a_pwm.start(0)
-            self.b_pwm.start(0)
-        else:
-            for pin in [a_pin, b_pin]:
-                GPIO.output(pin, GPIO.LOW)
-
-    def speed(self, speed):
-        if speed > 100:
-            speed = 100
-        elif speed < -100:
-            speed = -100
-        if speed > 0:
-            self.forward(speed)
-        elif speed < 0:
-            self.backward(speed * -1)
-        else:
-            self.stop()
-
-    def forward(self, speed=100.0):
-        if self.pwm:
-            self.a_pwm.ChangeDutyCycle(speed)
-            self.b_pwm.ChangeDutyCycle(0)
-        else:
-            GPIO.output(self.a_pin, GPIO.HIGH)
-            GPIO.output(self.b_pin, GPIO.LOW)
-
-    def backward(self, speed=100.0):
-        if self.pwm:
-            self.a_pwm.ChangeDutyCycle(0)
-            self.b_pwm.ChangeDutyCycle(speed)
-        else:
-            GPIO.output(self.a_pin, GPIO.LOW)
-            GPIO.output(self.b_pin, GPIO.HIGH)
-
-    def stop(self):
-        if self.pwm:
-            self.a_pwm.ChangeDutyCycle(0)
-            self.b_pwm.ChangeDutyCycle(0)
-        else:
-            GPIO.output(self.a_pin, GPIO.LOW)
-            GPIO.output(self.b_pin, GPIO.LOW)
-
-    def close(self):
-        if self.pwm:
-            self.a_pwm.stop()
-            self.b_pwm.stop()
-
-
-class Button():
-    """Imitates joystick behavior by gradually increasing the value"""
-    max = 100
-    ramp_time = 0.5
-
-    def __init__(self):
-        self.current = False
-        self.last = False
-        self.value = 0
-
-    def update(self, now):
-        if now is not self.last:
-            self.last = now
-            self.value = 0
-            if now:
-                self.start_press = time.time()
-        else:
-            if not now:
-                self.value = 0
-            else:
-                if time.time() - self.start_press > self.ramp_time:
-                    self.value = self.max
-                else:
-                    factor = (time.time() - self.start_press) / self.ramp_time
-                    self.value = self.max * factor
-
-
 def control_motors():
+    normal_speed = 0.7
+    turn_speed = 0.5
+
     GPIO.setmode(GPIO.BCM)
     m1 = Motor(4, 18, pwm=True)
     m2 = Motor(12, 19, pwm=True)
-    normal = 0.7
-    turn = 0.5
     u = Button()
     d = Button()
     l = Button()
@@ -124,30 +32,29 @@ def control_motors():
                 motor1_speed = 0
                 motor2_speed = 0
                 if keys_dict['up arrow']:
-                    motor1_speed += u.value*normal
-                    motor2_speed += u.value*normal
+                    motor1_speed += u.value * normal_speed
+                    motor2_speed += u.value * normal_speed
                     if keys_dict['left arrow']:
-                        motor1_speed += l.value*turn
-                        motor2_speed -= l.value*turn
+                        motor1_speed += l.value * turn_speed
+                        motor2_speed -= l.value * turn_speed
                     elif keys_dict['right arrow']:
-                        motor1_speed -= r.value*turn
-                        motor2_speed += r.value*turn
+                        motor1_speed -= r.value * turn_speed
+                        motor2_speed += r.value * turn_speed
                 elif keys_dict['down arrow']:
-                    motor1_speed -= d.value*normal
-                    motor2_speed -= d.value*normal
+                    motor1_speed -= d.value * normal_speed
+                    motor2_speed -= d.value * normal_speed
                     if keys_dict['left arrow']:
-                        motor1_speed -= l.value*turn
-                        motor2_speed += l.value*turn
+                        motor1_speed -= l.value * turn_speed
+                        motor2_speed += l.value * turn_speed
                     elif keys_dict['right arrow']:
-                        motor1_speed += r.value*turn
-                        motor2_speed -= r.value*turn
+                        motor1_speed += r.value * turn_speed
+                        motor2_speed -= r.value * turn_speed
                 elif keys_dict['left arrow']:
-                    motor1_speed += l.value*turn
-                    motor2_speed -= l.value*turn
+                    motor1_speed += l.value * turn_speed
+                    motor2_speed -= l.value * turn_speed
                 elif keys_dict['right arrow']:
-                    motor1_speed -= r.value*turn
-                    motor2_speed += r.value*turn
-
+                    motor1_speed -= r.value * turn_speed
+                    motor2_speed += r.value * turn_speed
 
                 m1.speed(motor1_speed)
                 m2.speed(motor2_speed)
