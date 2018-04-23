97964770ccd3bb5b3f7914aaf043685f2a5d820e
Martin Loeland <martinloland@gmail.com>
2018-04-23 17:57:24 +0200

    experiment parser 2

--- examples_edurov/experiment/data_process.py
+++ examples_edurov/experiment/data_process.py
@@ -1,5 +1,6 @@
-def response_parser(path):
-    print(path)
+def response_parser(not_used, path):
+    print('not ' + not_used)
+    print('path ' + path)
     if path.startswith('new_participant'):
         print('yep')
     return None
