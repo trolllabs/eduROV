a04bd5b93104877b564d32edb99323ce918a2476
Martin Loeland <martinloland@gmail.com>
2018-04-23 18:11:10 +0200

    experiment parser 6

--- examples_edurov/experiment/data_process.py
+++ examples_edurov/experiment/data_process.py
@@ -1,4 +1,9 @@
 def response_parser(not_used, path):
     if path.startswith('/new_participant'):
-        form_data = path[path.find('?') + 1:]
-        print(form_data.split('&'))
+        form_raw = path[path.find('?') + 1:].split('&')
+        form_data = {}
+        for field in form_raw:
+            pair = field.split('=')
+            form_data.update({pair[0]:pair[1]})
+        print(form_data)
+    return 'redirect=/'
