d6b43677044b0e8c862fb28e5002b6c12c65006c
Martin Loeland <martinloland@gmail.com>
2018-04-23 18:07:59 +0200

    experiment parser 5

--- examples_edurov/experiment/data_process.py
+++ examples_edurov/experiment/data_process.py
@@ -1,10 +1,4 @@
 def response_parser(not_used, path):
-    if isinstance(not_used,str):
-        print('wow')
-    if isinstance(path, str):
-        print('path ' + path)
-        if path.startswith('new_participant'):
-            print('yep')
-        return None
-    else:
-        print('nope')
+    if path.startswith('/new_participant'):
+        form_data = path[path.find('?') + 1:]
+        print(form_data.split('&'))
