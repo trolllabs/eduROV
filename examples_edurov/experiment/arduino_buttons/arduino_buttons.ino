d020140cd17bc6ee76227e3bad6b99761d30eb5a
Martin Loeland <martinloland@gmail.com>
2018-05-01 14:06:30 +0200

    added arduino code

---
+++ examples_edurov/experiment/arduino_buttons/arduino_buttons.ino
@@ -0,0 +1,36 @@
+int R1pin = A0;
+int R2pin = A1;
+int R3pin = A2;
+
+int led1pin = 2;
+int led2pin = 3;
+int led3pin = 4;
+
+int R1;
+int R2;
+int R3;
+
+String msg;
+
+void setup() {
+  pinMode(R1pin, INPUT);
+  pinMode(R2pin, INPUT);
+  pinMode(R3pin, INPUT);
+  
+  pinMode(led1pin, OUTPUT);
+  pinMode(led2pin, OUTPUT);
+  pinMode(led3pin, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  R1 = analogRead(R1pin);
+  R2 = analogRead(R2pin);
+  R3 = analogRead(R3pin);
+
+  Serial.print(R1 + "  ");
+  Serial.print(R2 + "  ");
+  Serial.println(R3);
+  delay(1000);
+
+}
