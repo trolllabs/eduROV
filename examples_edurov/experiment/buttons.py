a19a3542340bdba88b848035ea197e197d1422d5
Martin Loeland <martinloland@gmail.com>
2018-05-01 21:42:54 +0200

    usb conn 2

--- examples_edurov/experiment/buttons.py
+++ examples_edurov/experiment/buttons.py
@@ -5,8 +5,8 @@ import requests
 from edurov.utils import serial_connection, receive_arduino_simple
 
 
-def main(server_ip, server_port):
-    ser = serial_connection(baudrate=9600, port='/dev/ttyUSB0')
+def main(server_ip, server_port, serial_port):
+    ser = serial_connection(baudrate=9600, port='/dev/{}'.format(serial_port))
     msg = receive_arduino_simple(serial_connection=ser, min_length=5)
     if msg:
         button = msg.split('=')[1]
@@ -33,6 +33,11 @@ if __name__ == '__main__':
         default=8000,
         help='port of webserver',
         type=str)
+    parser.add_argument(
+        '-ser',
+        default='ttyAMA0',
+        help='serial port',
+        type=str)
 
     args = parser.parse_args()
-    main(server_ip=args.ip, server_port=args.port)
+    main(server_ip=args.ip, server_port=args.port, serial_port=args.ser)
