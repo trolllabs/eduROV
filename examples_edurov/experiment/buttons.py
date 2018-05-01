83ab3ed18ac693fc010fe3b76feef8e1de105c36
Martin Loeland <martinloland@gmail.com>
2018-05-01 21:50:45 +0200

    usb conn 4

--- examples_edurov/experiment/buttons.py
+++ examples_edurov/experiment/buttons.py
@@ -36,7 +36,7 @@ if __name__ == '__main__':
         type=str)
     parser.add_argument(
         '-ser',
-        default='ttyAMA0',
+        default='ttyUSB0',
         help='serial port',
         type=str)
 
