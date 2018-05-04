d8fea6d7611c7e7cc54d571f5ecb8fee45ae7569
Martin Loeland <martinloland@gmail.com>
2018-05-05 01:35:45 +0200

    next bug 13

--- examples_edurov/experiment/displays/experiment0.js
+++ examples_edurov/experiment/displays/experiment0.js
@@ -1,32 +1,32 @@
-window.alert("Will now get 30 seconds to try this display");
-
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
-
+//window.alert("Will now get 30 seconds to try this display");
+//
+//function sleep(ms) {
+//  return new Promise(resolve => setTimeout(resolve, ms));
+//}
+//
+////function next_page(){
+////    var xhttp = new XMLHttpRequest();
+////    xhttp.open("GET", "/next", true);
+////    xhttp.setRequestHeader("Content-Type", "text/html");
+////    xhttp.send();
+////}
+//
+////window.location.replace("http://stackoverflow.com");
+//
 //function next_page(){
 //    var xhttp = new XMLHttpRequest();
+//    xhttp.onreadystatechange = function() {
+//            if (this.readyState == 4 && this.status == 200) {
+//                console.log(this.responseText)
+//            }
+//        };
 //    xhttp.open("GET", "/next", true);
-//    xhttp.setRequestHeader("Content-Type", "text/html");
 //    xhttp.send();
 //}
-
-//window.location.replace("http://stackoverflow.com");
-
-function next_page(){
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-                console.log(this.responseText)
-            }
-        };
-    xhttp.open("GET", "/next", true);
-    xhttp.send();
-}
-
-
-console.log('Before')
-sleep(4000)
-console.log('After')
-
-next_page()
+//
+//
+//console.log('Before')
+//sleep(5000)
+//console.log('After')
+//
+//next_page()
--- examples_edurov/experiment/displays/experiment1.js
+++ examples_edurov/experiment/displays/experiment1.js
@@ -1,16 +1,16 @@
-window.alert("Will now get 30 seconds to try this display");
-
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-function next_page(){
-    var xhttp = new XMLHttpRequest();
-    xhttp.open("GET", "/next", true);
-    xhttp.setRequestHeader("Content-Type", "text/html");
-    xhttp.send();
-}
-
-sleep(4000)
-
-next_page()
+//window.alert("Will now get 30 seconds to try this display");
+//
+//function sleep(ms) {
+//  return new Promise(resolve => setTimeout(resolve, ms));
+//}
+//
+//function next_page(){
+//    var xhttp = new XMLHttpRequest();
+//    xhttp.open("GET", "/next", true);
+//    xhttp.setRequestHeader("Content-Type", "text/html");
+//    xhttp.send();
+//}
+//
+//sleep(4000)
+//
+//next_page()
