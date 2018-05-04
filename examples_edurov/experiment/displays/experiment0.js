ab9718bdf7038eb3eee9c8867a477e5b35b3f631
Martin Loeland <martinloland@gmail.com>
2018-05-05 01:10:08 +0200

    next bug 8

--- examples_edurov/experiment/displays/experiment0.js
+++ examples_edurov/experiment/displays/experiment0.js
@@ -1,5 +1,16 @@
-if (confirm("Press a button!")) {
-    txt = "You pressed OK!";
-} else {
-    txt = "You pressed Cancel!";
+window.alert("Will now get 30 seconds to try this display");
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
+
+function next_page(){
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("GET", "next", true);
+    xhttp.setRequestHeader("Content-Type", "text/html");
+    xhttp.send(json_string);
+}
+
+sleep(4000)
+
+next_page()
