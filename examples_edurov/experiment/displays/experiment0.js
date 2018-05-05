f6df62ac5c02881a5866bad604c54b95159f12e7
Martin Loeland <martinloland@gmail.com>
2018-05-05 12:06:28 +0200

    timer 2

--- examples_edurov/experiment/displays/experiment0.js
+++ examples_edurov/experiment/displays/experiment0.js
@@ -1,11 +1,13 @@
-//window.alert("Will now get 30 seconds to try this display");
-//
-//function sleep(ms) {
-//  return new Promise(resolve => setTimeout(resolve, ms));
-//}
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 
-var total_time = 30;
+var total_time = 10;
 var elapsed = 0;
+
+sleep(1000)
+
+window.alert("Will now get 30 seconds to try this display");
 
 var x = setInterval(function() {
     if (total_time > elapsed){
@@ -13,5 +15,6 @@ var x = setInterval(function() {
         document.getElementById("timer").innerHTML = total_time-elapsed;
     } else {
         document.getElementById("timer").innerHTML = "EXPIRED";
+        window.alert("Click OK to enter the experiment, 1 min");
     }
 }, 1000);
