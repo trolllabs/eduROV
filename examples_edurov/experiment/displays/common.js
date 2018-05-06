9005f30df214f43b9761b14d2cc3566b2ccfbbc8
Martin Loeland <martinloland@gmail.com>
2018-05-06 21:55:16 +0200

    faster key send 6

--- examples_edurov/experiment/displays/common.js
+++ examples_edurov/experiment/displays/common.js
@@ -8,7 +8,6 @@ var arrow_key_codes = [38, 40, 39, 37];
 var training = true;
 var experimenting = false;
 var last_key;
-var key_dict = {event:'', keycode:0};
 
 function determine_exp(){
     var url = window.location.href;
@@ -26,30 +25,22 @@ function update_exp1_keys(keycode, value){
 }
 
 document.onkeydown = async function(evt) {
-    console.log('key DOWN')
     evt = evt || window.event;
     if (evt.keyCode != last_key){
         if (arrow_key_codes.indexOf(evt.keyCode) > -1){
-    //        key_dict['event'] = 'KEYDOWN';
-    //        key_dict['keycode'] = evt.keyCode;
             last_key = evt.keyCode;
             if (experimenting || training){
                 if (exp == 1){
                     update_exp1_keys(evt.keyCode, 1)
                 }
                 await sleep(added_delay);
-    //            send_keys(JSON.stringify(key_dict));
                 send_keydown(evt.keyCode);
-                console.log('sent DOWN')
             }
         }
     }
 }
 
 document.onkeyup = async function(evt) {
-    console.log('key UP')
-//    key_dict['event'] = 'KEYUP';
-//    key_dict['keycode'] = evt.keyCode;
     last_key = 0;
     if (arrow_key_codes.indexOf(evt.keyCode) > -1){
         if (experimenting || training){
@@ -57,19 +48,8 @@ document.onkeyup = async function(evt) {
                 update_exp1_keys(evt.keyCode, 0)
             }
             await sleep(added_delay);
-    //        send_keys(JSON.stringify(key_dict));
             send_keyup(evt.keyCode);
-            console.log('sent UP')
         }
-    }
-}
-
-function send_keys(json_string){
-    if(json_string.length > 0){
-        var xhttp = new XMLHttpRequest();
-        xhttp.open("POST", "/keys.json", true);
-        xhttp.setRequestHeader("Content-Type", "application/json");
-        xhttp.send(json_string);
     }
 }
 
--- examples_edurov/experiment/displays/predictive.js
+++ examples_edurov/experiment/displays/predictive.js
@@ -7,8 +7,8 @@ var key_status = {up: 0, down: 0, right: 0, left:0}
 var horizontal_move = 0;
 var vertical_move = 0;
 
-var update_interval = 50;
-var perceived_delay = 800;
+var update_interval = 25;
+var perceived_delay = 750;
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
@@ -27,5 +27,6 @@ var x = setInterval(function() {
         horizontal_move -= 1;
         update_hor_with_delay(+1, perceived_delay);
     }
+    console.log(horizontal_move)
 }, update_interval);
 
