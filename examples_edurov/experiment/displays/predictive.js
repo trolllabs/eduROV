bb0ebf39fdc7a31a2b8e653b5cd70ac952ac780b
Martin Loeland <martinloland@gmail.com>
2018-05-06 22:15:14 +0200

    predictor movement 4

--- examples_edurov/experiment/displays/predictive.js
+++ examples_edurov/experiment/displays/predictive.js
@@ -3,7 +3,8 @@ var down = 40;
 var right = 39;
 var left = 37;
 var key_status = {up: 0, down: 0, right: 0, left:0};
-var base_marginLeft = 300;
+var base_margin = 300;
+var pixel_turn_rate = 10;
 
 var horizontal_move = 0;
 var vertical_move = 0;
@@ -30,7 +31,15 @@ var x = setInterval(function() {
         update_hor_with_delay(+1, perceived_delay);
     }
     console.log(horizontal_move);
-    horizontal_px_move = base_marginLeft+horizontal_move;
+    horizontal_px_move = base_margin+horizontal_move*pixel_turn_rate;
     document.getElementById("stream").style.marginLeft = `${horizontal_px_move}px`;
 }, update_interval);
 
+function set_base_margin(){
+    var myImage = new Image();
+    var img = document.getElementById("stream");
+    myImage.src = img.src;
+    var imgW = myImage.width;
+    var bodW = document.body.clientWidth;
+    base_margin = (bodW-imgW)/2;
+}
