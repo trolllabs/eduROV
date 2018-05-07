98735d5a2b41a0b58155ce684d2c2bd319f87983
Martin Loeland <martinloland@gmail.com>
2018-05-07 20:07:36 +0200

    removed note

--- examples_edurov/experiment/displays/common.js
+++ examples_edurov/experiment/displays/common.js
@@ -28,7 +28,6 @@ function update_exp1_keys(keycode, value){
 }
 
 document.onkeydown = async function(evt) {
-    document.getElementById("note").innerHTML = "<h1>DOWN</h1>";
     evt = evt || window.event;
     if (evt.keyCode != last_key){
         last_key = evt.keyCode;
@@ -45,7 +44,6 @@ document.onkeydown = async function(evt) {
 }
 
 document.onkeyup = async function(evt) {
-    document.getElementById("note").innerHTML = "";
     last_key = 0;
     if (experimenting || training){
         if (exp == 1){
